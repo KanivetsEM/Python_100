a533a644ce6c507afaa752617d2fa75f1748e18b
KanivetsEM <115340690+KanivetsEM@users.noreply.github.com>
2022-10-28 18:11:37 +0300

    Add files via upload

---
+++ 2-1.py
@@ -0,0 +1,11 @@
+list_ = [8, 9, -5, -3, 1, -10, 8, -10, -5, 0, 5, -4, 0, 10, -8, 1, 6, -6, 6, -9]
+unicue=set(list_)
+
+sum_=sum(unicue)
+len_=len(unicue)
+
+print(sum_)
+print(len_)
+
+print(round(sum_/len_,5))
+# TODO найти сумму, количество и среднее арифметическое уникальных чисел
