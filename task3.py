091af2311a77e8c4de03afcf4928cfa1416bab68
KanivetsEM <115340690+KanivetsEM@users.noreply.github.com>
2022-11-08 21:49:22 +0300

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+
+def delete(list_, index: Union[None, int] = None):
+    if index is None:
+        return list_[:-1]
+    return list_[:index] + list_[index + 1:]
+
+
+def delete(list_, index: Union[None, int] = None):
+    if index is None:
+        del list_[-1]
+        return list_
+    del list_[index]
+    return list_
+
+
+print(delete([0, 0, 1, 2], index=0))  # [0, 1]
+print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2]
+print(delete([0, 1, 2, 3, 4, 4]))  # [0, 1, 2, 3]
